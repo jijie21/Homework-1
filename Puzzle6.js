faf6e72d64c8e657f6e6b1ba0ccd61dd98f24454
jijie21 <35789241+jijie21@users.noreply.github.com>
2018-01-26 14:54:09 -0800

    Create Puzzle6.js

---
+++ Puzzle6.js
@@ -0,0 +1,34 @@
+right();
+
+var a = getColor();
+function movea (){
+  if (getColor() == "blue") {
+  down();
+  down();
+  down();
+  if (getColor() == "red") {
+  left();
+  down();
+  }
+ }
+}
+
+function moveb(){  
+  if (getColor()== "red") {
+  up();
+  up();
+  up();
+  if (getColor() == "blue") {
+  right();
+  up();
+  }
+  if (getColor() == "red"){
+  left();
+  up();
+  }
+}
+}
+
+
+movea();
+moveb();
