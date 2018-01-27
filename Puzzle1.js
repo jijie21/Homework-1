297da8d655f25cf1439d1478159a2da4a54ac072
jijie21 <35789241+jijie21@users.noreply.github.com>
2018-01-26 18:41:57 -0800

    Update Puzzle1.js

--- Puzzle1.js
+++ Puzzle1.js
@@ -1,15 +1,28 @@
-down
-down
-down
-down
-down
-down
-down
-right
-right
-right
-right
-right
-up
-right
-up
+function repeatDown(total) {
+  var count = 0;
+  while (count < total) {
+    down();
+    count = count + 1;
+  }
+}
+
+function repeatUp(total) {
+  var count = 0;
+  while (count < total) {
+    up();
+    count = count + 1;
+  }
+}
+  
+function repeatRight(total) {
+  var count = 0;
+  while (count < total) {
+    right();
+    count = count + 1;
+  }
+}
+
+repeatDown(7);
+repeatRight(5);
+repeatUp(2);
+repeatRight(2);
